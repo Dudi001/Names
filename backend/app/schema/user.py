eacbb05a7bb65ad6a21c7f75c29df9f998df84ed
Dudi001 <murad.manapov.96@gmail.com>
2025-05-14 23:19:47 +0300

    Поправил валидацию данных

--- backend/app/schema/user.py
+++ backend/app/schema/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field, root_validator, validator
+from pydantic import BaseModel, Field, root_validator, EmailStr
 from typing import Optional
 from fastapi.exceptions import HTTPException
 
@@ -8,33 +8,27 @@ class UserCreate(BaseModel):
     password_hash: str = Field(..., min_length=5)
     first_name: str
     last_name: Optional[str]
-    email: str
+    email: EmailStr = Field(..., description="User email")
 
     @root_validator(pre=True)
     def pre_validator(cls, values):
         keys = values.keys()
         if "username" not in keys or not values.get("username"):
             raise HTTPException(
-                400, "Отсутствуют логин пользователя"
+                400,
+                "Отсутствует логин пользователя"
             )
 
-        if "first_name" not in keys or values["first_name"] is None:
+        if "first_name" not in keys or not values.get("first_name"):
             raise HTTPException(
-                400, "Отсутствуют имя пользователя"
+                400,
+                "Отсутствуют имя пользователя"
             )
 
-        if "email" not in keys or values["email"] is None:
+        if "email" not in keys or not values.get("email"):
             raise HTTPException(
-                400, "Отсутствуют email пользователя"
+                400,
+                "Отсутствуют email пользователя"
             )
 
         return values
-
-    @validator("password_hash", pre=True)
-    def id_validator(cls, value):
-        try:
-            value >= 5
-        except (ValueError, TypeError):
-            raise HTTPException(400, "Пароль должен быть боле 5 символов")
-        return value
-
